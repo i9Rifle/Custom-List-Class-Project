f24468bec61b655fd3cda8ccaf9941fb802032db
Matthew Acker <matthew.acker11b@gmail.com>
2020-01-23 14:55:10 -0600

    created 5 tests for Add method

---
+++ CustomlistTesting/AddUnitTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListClassProj;
+
+namespace CustomListTesting
+{
+    [TestClass]
+    public class AddUnitTests
+    {
+        [TestMethod]
+        public void Add_TwoValues_Count()
+        {
+            //arrange
+            CustomList<int> MyList = new CustomList<int>();
+            int value1 = 5;
+            int value2 = 10;
+            int expected = 2;
+            int actual;
+            //act
+            MyList.Add(value1);
+            MyList.Add(value2);
+            actual = MyList.Count;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_TwoValues_SecondIndex()
+        {
+            //arrange
+            CustomList<int> MyList = new CustomList<int>();
+            int value1 = 5;
+            int value2 = 10;
+            int expected = 10;
+            int actual;
+            //act
+            MyList.Add(value1);
+            MyList.Add(value2);
+            actual = MyList[1];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        public void Add_FiveValues_Count()
+        {
+            //arange
+            CustomList<int> MyList = new CustomList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int value3 = 3;
+            int value4 = 4;
+            int value5 = 5;
+            int expected = 5;
+            int actual;
+            //act
+            MyList.Add(value1);
+            MyList.Add(value2);
+            MyList.Add(value3);
+            MyList.Add(value4);
+            MyList.Add(value5);
+            actual = MyList.Count;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        public void Add_FiveValues_IndexAtTwo()
+        {
+            //arange
+            CustomList<int> MyList = new CustomList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int value3 = 3;
+            int value4 = 4;
+            int value5 = 5;
+            int expected = 3;
+            int actual;
+            //act
+            MyList.Add(value1);
+            MyList.Add(value2);
+            MyList.Add(value3);
+            MyList.Add(value4);
+            MyList.Add(value5);
+            actual = MyList[2];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        public void Add_FiveValues_IncreasedArrayCapacity()
+        {
+            //
+            CustomList<int> MyList = new CustomList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int value3 = 3;
+            int value4 = 4;
+            int value5 = 5;
+            int expected = 8;
+            int actual;
+            //act
+            MyList.Add(value1);
+            MyList.Add(value2);
+            MyList.Add(value3);
+            MyList.Add(value4);
+            MyList.Add(value5);
+            actual = MyList.Capacity;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CustomlistTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomlistTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomlistTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f23f22e8-7654-4149-8461-0893f5d953f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
