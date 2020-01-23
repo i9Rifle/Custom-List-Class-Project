428e43beb9757e47b998ab44a5b036a8cde23560
Matthew Acker <matthew.acker11b@gmail.com>
2020-01-23 16:20:12 -0600

    created customList class and created AddMethod

--- CustomListClassProj/CustomList.cs
+++ CustomListClassProj/CustomList.cs
@@ -8,6 +8,25 @@ namespace CustomListClassProj
 {
     public class CustomList<T>
     {
+        T[] items;
+        int capacity;
+        int count;
 
+        public CustomList()
+        {
+            count = 0;
+            capacity = 4;
+            items = new T[4];
+        }
+        public void Add(T item)
+        {
+            items[count] = item;
+            count++;
+            if(count >= capacity)
+            {
+                capacity *= 2;
+                T[] tempArray = new T[capacity];
+            }
+        }
     }
 }
